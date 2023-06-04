98096be7bdc2c63b8f9fc8ab8ca5f2c6a05a6632
Burcu Taş <burcutas636@gmail.com>
2023-06-04 18:06:32 +0300

    header section

--- js/custom/script.js
+++ js/custom/script.js
@@ -12,3 +12,43 @@ for (let i = 0; i < BtnCanvas.length; i++) {
     });
 }
 
+
+
+function checkScreenWidth(){
+    var element = document.querySelector(".my");
+    if (window.innerWidth > 768){
+        if (!element.classList.contains("anim-typewriter")) {    
+            element.classList.add("anim-typewriter");
+            element.classList.remove("anim-typewriter-small");
+            element.classList.remove("anim-typewriter-xsmall");
+            element.classList.remove("anim-typewriter-xxsmall");
+        }
+    }
+    else if(window.innerWidth > 578 && window.innerWidth <= 768){
+        if (!element.classList.contains("anim-typewriter-small")) {    
+            element.classList.add("anim-typewriter-small");
+            element.classList.remove("anim-typewriter");
+            element.classList.remove("anim-typewriter-xsmall");
+            element.classList.remove("anim-typewriter-xxsmall");
+        }
+    }
+    else if(window.innerWidth > 405 && window.innerWidth <= 578){
+        if (!element.classList.contains("anim-typewriter-xsmall")) {    
+            element.classList.add("anim-typewriter-xsmall");
+            element.classList.remove("anim-typewriter");
+            element.classList.remove("anim-typewriter-small");
+            element.classList.remove("anim-typewriter-xxsmall");
+        }
+    }
+    else if(window.innerWidth <= 405){
+        if (!element.classList.contains("anim-typewriter-xxsmall")) {    
+            element.classList.add("anim-typewriter-xxsmall");
+            element.classList.remove("anim-typewriter");
+            element.classList.remove("anim-typewriter-small");
+            element.classList.remove("anim-typewriter-xsmall");
+        }
+    }
+}
+checkScreenWidth();
+window.addEventListener("resize", checkScreenWidth);
+
