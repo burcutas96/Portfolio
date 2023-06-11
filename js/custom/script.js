d1e6d38d03ffdd893647d75563946a132aa6c5a7
Burcu Taş <burcutas636@gmail.com>
2023-06-11 17:07:34 +0300

    scroll-bar

--- js/custom/script.js
+++ js/custom/script.js
@@ -8,6 +8,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+//scroll-up
+let calcScrollValue = () =>{
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = 
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+
+    if(pos > 100){
+        scrollProgress.style.display = "grid";
+    }
+    else{
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = 
+    `conic-gradient(#262323 ${scrollValue}%, #aaaaaa ${scrollValue}% )`;
+}
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
+
+
 //Kullanıcının mobil cihaz ile girip girmediğini kontrol ediyoruz.
 function checkDevice(){
     var isMobile = window.matchMedia("(max-width: 768px)").matches;
