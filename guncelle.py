5f47166b5605009616271408f2bb03b85cb642cb
keyiflerolsun <keyiflerolsun@gmail.com>
2023-06-25 11:23:40 +0300

    🕊
    🕊

--- guncelle.py
+++ guncelle.py
@@ -12,7 +12,8 @@ repolar = [
         if repo.get("language") and repo.get("description")
 ]
 repolar = sorted(repolar, key=lambda veri: veri["stargazers_count"], reverse=True)
-
+from Kekik.cli import konsol
+konsol.print(repolar[0])
 with open("__index.html", "r", encoding="utf-8") as dosya:
     eldeki_dosya = dosya.read()
 
@@ -44,13 +45,13 @@ eklenecek_metin = "".join(
             </div>
             <div class="project-footer-right">
                 <i class="bi bi-star"></i>
-                <span class="point me-3">6</span>
+                <span class="point me-3">{repo.get("stargazers_count")}</span>
                 <svg aria-hidden="true" height="16" viewBox="0 0 24 24" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo-forked blankslate-icon">
                     <path d="M8.75 19.25a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0ZM15 4.75a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0Zm-12.5 0a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0ZM5.75 6.5a1.75 1.75 0 1 0-.001-3.501A1.75 1.75 0 0 0 5.75 6.5ZM12 21a1.75 1.75 0 1 0-.001-3.501A1.75 1.75 0 0 0 12 21Zm6.25-14.5a1.75 1.75 0 1 0-.001-3.501A1.75 1.75 0 0 0 18.25 6.5Z"></path>
                     <path d="M6.5 7.75v1A2.25 2.25 0 0 0 8.75 11h6.5a2.25 2.25 0 0 0 2.25-2.25v-1H19v1a3.75 3.75 0 0 1-3.75 3.75h-6.5A3.75 3.75 0 0 1 5 8.75v-1Z"></path>
                     <path d="M11.25 16.25v-5h1.5v5h-1.5Z"></path>
                 </svg>
-                <span class="point">0</span>
+                <span class="point">{repolar.get("forks")}</span>
             </div>
         </div>
     </div>
